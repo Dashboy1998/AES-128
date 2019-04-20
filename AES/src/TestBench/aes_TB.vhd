1c1cfe154879d38a5f0689dfc4b3c5ed9c5db2da
Dashboy1998 <dashboy1998@gmail.com>
2019-04-20 11:53:53 -0400

    Added test bench

---
+++ AES/src/TestBench/aes_TB.vhd
@@ -0,0 +1,76 @@
+library aes;
+library ieee;
+use ieee.std_logic_1164.all;
+use aes.data_types.all;
+use aesTest_GRBC_s18.all;
+
+	-- Add your library and packages declaration here ...
+
+entity aes_tb is
+end aes_tb;
+
+architecture TB_ARCHITECTURE of aes_tb is
+	-- Component declaration of the tested unit
+	component aes
+	port(
+		data : in STD_LOGIC_VECTOR(127 downto 0);
+		key : in STD_LOGIC_VECTOR(127 downto 0);
+		ED : in STD_LOGIC;
+		Xout : out STD_LOGIC_VECTOR(127 downto 0);
+		Yout : out STD_LOGIC_VECTOR(127 downto 0) );
+	end component;
+
+	-- Stimulus signals - signals mapped to the input and inout ports of tested entity
+	signal data : STD_LOGIC_VECTOR(127 downto 0);
+	signal key : STD_LOGIC_VECTOR(127 downto 0);
+	signal ED : STD_LOGIC;
+	-- Observed signals - signals mapped to the output ports of tested entity
+	signal Xout : STD_LOGIC_VECTOR(127 downto 0);
+	signal Yout : STD_LOGIC_VECTOR(127 downto 0);
+
+	-- Add your code here ...
+	signal correct: boolean;
+begin
+
+	-- Unit Under Test port map
+	UUT : aes
+		port map (
+			data => data,
+			key => key,
+			ED => ED,
+			Xout => Xout,
+			Yout => Yout
+		);
+
+	-- Add your stimulus here ...
+	process
+	begin
+		for i in tests'range loop -- Nth test
+			key <= tests(i).key;
+			-- Encryption
+			ED <= '0';
+			data <= tests(i).plain;
+			wait for 10ns;
+			correct <= (tests(i).expected = Xout);
+			-- Decryption
+			ED <= '1';
+			data <= tests(i).expected;
+			wait for 10ns;
+			correct <= (tests(i).plain = Xout);
+		end loop;
+		wait for 10ns;
+				assert false
+		report "End of testing"
+		severity failure;
+	end process;
+	
+end TB_ARCHITECTURE;
+
+configuration TESTBENCH_FOR_aes of aes_tb is
+	for TB_ARCHITECTURE
+		for UUT : aes
+			use entity work.aes(dataflow);
+		end for;
+	end for;
+end TESTBENCH_FOR_aes;
+
---
+++ AES/src/TestBench/aes_TB_runtest.do
@@ -0,0 +1,35 @@
+SetActiveLib -work
+comp -include "$dsn\src\Data_Types.vhd" 
+comp -include "$dsn\src\Key_Expansion\Rcon_Table.vhd" 
+comp -include "$dsn\src\Sbox_Transformation.vhd" 
+comp -include "$dsn\src\int.vhd" 
+comp -include "$dsn\src\Encryption_Decryption\Mult_Tables.vhd" 
+comp -include "$dsn\src\Encryption_Decryption\EDRotate.vhd" 
+comp -include "$dsn\src\Encryption_Decryption\Esbox.vhd" 
+comp -include "$dsn\src\Encryption_Decryption\EMixColumns.vhd" 
+comp -include "$dsn\src\Decryption\Decryption.vhd" 
+comp -include "$dsn\src\Decryption\Decryption_Final.vhd" 
+comp -include "$dsn\src\Encryption_Decryption\Cryption.vhd" 
+comp -include "$dsn\src\Encryption_Decryption\Cryption_Final.vhd" 
+comp -include "$dsn\src\Key_Expansion\XKey.vhd" 
+comp -include "$dsn\src\Key_Expansion\XRCON.vhd" 
+comp -include "$dsn\src\Key_Expansion\sbox.vhd" 
+comp -include "$dsn\src\Key_Expansion\Key_Rotate.vhd" 
+comp -include "$dsn\src\Key_Expansion\KRound.vhd" 
+comp -include "$dsn\src\decryption\decryptionrounds.vhd" 
+comp -include "$dsn\src\Encryption_Decryption\CryptionRounds.vhd" 
+comp -include "$dsn\src\Key_Expansion\KExpansion.vhd" 
+comp -include "$dsn\src\AES.vhd" 
+comp -include "$dsn\src\TestBench\aes_TB.vhd" 
+asim +access +r TESTBENCH_FOR_aes 
+wave 
+wave -noreg data
+wave -noreg key
+wave -noreg ED
+wave -noreg Xout
+wave -noreg Yout
+wave -noreg correct
+# The following lines can be used for timing simulation
+# acom <backannotated_vhdl_file_name>
+# comp -include "$dsn\src\TestBench\aes_TB_tim_cfg.vhd" 
+# asim +access +r TIMING_FOR_aes 
