0affcc3d668b1d06d7b94521c00c3ff5ea87d380
Dashboy1998 <dashboy1998@gmail.com>
2019-04-20 03:06:03 -0400

    Rotate now working

--- AES/src/Data_Types.vhd
+++ AES/src/Data_Types.vhd
@@ -8,21 +8,62 @@ package data_types is
 	type AByte is array (natural range<>) of byte;
 	type AAByte is array (natural range<>) of AByte;
 	subtype word is std_logic_vector(31 downto 0);
+	type AWord is array (0 to 3) of word;
 	subtype nRound is natural range 0 to numRounds;
 	type ARounds is array (0 to numRounds) of std_logic_vector(127 downto 0);
-	function to_AAByte(X: std_logic_vector(127 downto 0)) return AAByte;
+	function to_AWord(X: std_logic_vector(127 downto 0)) return AWord;
+	function to_stdvect(X: AWord) return std_logic_vector;
 end data_types;	   	 
 
 package body data_types is
-	function to_AAByte(X: std_logic_vector(127 downto 0)) 
-	return AAByte is
-	variable Y: AAByte(0 to 3)(0 to 3);
+	function to_AWord(X: std_logic_vector(127 downto 0))
+		return AWord is
+		variable Y: AWord;
+		alias S00 is X(127 downto 120);
+		alias S01 is X(119 downto 112);
+		alias S02 is X(111 downto 104);
+		alias S03 is X(103 downto  96);
+		alias S04 is X( 95 downto  88);
+		alias S05 is X( 87 downto  80);
+		alias S06 is X( 79 downto  72);
+		alias S07 is X( 71 downto  64);
+		alias S08 is X( 63 downto  56);
+		alias S09 is X( 55 downto  48);
+		alias S10 is X( 47 downto  40);
+		alias S11 is X( 39 downto  32);
+		alias S12 is X( 31 downto  24);
+		alias S13 is X( 23 downto  16);
+		alias S14 is X( 15 downto   8);
+		alias S15 is X(  7 downto   0);
 	begin
-		Column: for j in 0 to 3 loop -- Columns
-			Row: for i in 0 to 3 loop -- Rows
-				Y(i)(j) := X((127-(j*8)-(i*32)) downto (120-(j*8)-(i*32)));
-			end loop Row;
-		end loop Column;
+		Y(0) := S00 & S04 & S08 & S12;
+		Y(1) := S01 & S05 & S09 & S13;
+		Y(2) := S02 & S06 & S10 & S14;
+		Y(3) := S03 & S07 & S11 & S15;
 		return Y;
-	end to_AAByte; 
+	end to_AWord;
+	
+	function to_stdvect(X: AWord) 
+	return std_logic_vector is
+		variable Y: std_logic_vector(127 downto 0);
+		alias S00 is X(0)(31 downto 24);
+		alias S01 is X(1)(31 downto 24);
+		alias S02 is X(2)(31 downto 24);
+		alias S03 is X(3)(31 downto 24);
+		alias S04 is X(0)(23 downto 16);
+		alias S05 is X(1)(23 downto 16);
+		alias S06 is X(2)(23 downto 16);
+		alias S07 is X(3)(23 downto 16);
+		alias S08 is X(0)(15 downto  8);
+		alias S09 is X(1)(15 downto  8);
+		alias S10 is X(2)(15 downto  8);
+		alias S11 is X(3)(15 downto  8);
+		alias S12 is X(0)( 7 downto  0);
+		alias S13 is X(1)( 7 downto  0);
+		alias S14 is X(2)( 7 downto  0);
+		alias S15 is X(3)( 7 downto  0);
+	begin
+		Y := S00 & S01 & S02 & S03 & S04 & S05 & S06 & S07 & S08 & S09 & S10 & S11 & S12 & S13 & S14 & S15;
+		return Y;
+	end to_stdvect;
 end package body data_types;
--- AES/src/Encryption_Decryption/EDRotate.vhd
+++ AES/src/Encryption_Decryption/EDRotate.vhd
@@ -5,25 +5,22 @@ use work.data_types.all;
 entity EDRotate is
 	port(
 		s: in std_logic_vector(127 downto 0);
-		r: out AAByte(0 to 3)(0 to 3);
+		r: out AWord;
 		ED: in std_logic
 		);
 end entity EDRotate;
 
 architecture dataflow of EDRotate is
-	signal forward: std_logic_vector(127 downto 0);
-	signal inverse: std_logic_vector(127 downto 0);	
-	signal tempR: AAByte(0 to 3)(0 to 3); 
+	signal matrix: AWord; 
 begin
-	tempR <= to_AAByte(s);
-	process(tempR, ED)
-	begin
-		r(0) <= tempR(0);
-		for j in 1 to 3 loop -- Rows
+	matrix <= to_AWord(s);
+	process(matrix, ED)
+	begin 
+		for i in 0 to 3 loop -- Rows
 			if(ED = '0') then -- Rotate left
-				
+				r(i) <= matrix(i) rol (i * 8);
 			else -- Rotate right
-				
+				r(i) <= matrix(i) ror (i * 8);
 			end if;
 		end loop;	
 	end process;
